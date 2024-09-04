88288bde1325adc93383b7fe775b72a3827ad4df
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-04 10:19:19 +0000

    Sep 4, 2024, 3:19 PM

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ setInterval(() => {
   let timeBox = document.querySelector(".dtime");
   hoursBox.innerText =
     date.getHours() > 12
-      ? date.getHours() < 10
+      ? (date.getHours() < 10
         ? "0" + (date.getHours() - 12)
-        : date.getHours()
-      : date.getHours() < 10
+        : date.getHours())
+      : (date.getHours() < 10
       ? "0" + date.getHours()
-      : date.getHours();
+      : date.getHours());
   minutesBox.innerText =
     date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
   secondsBox.innerText =
