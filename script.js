b22e21804dcc4b9dde51e7d8bb85f48a049439cc
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-04 03:15:02 +0000

    Sep 4, 2024, 8:15 AM

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ setInterval(() => {
   let hour_needle = document.querySelector(".hours");
   let minute_needle = document.querySelector(".minutes");
   let second_needle = document.querySelector(".seconds");
-  // hour_needle.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
-  // minute_needle.style.transform = `rotate(${6 * minutes}deg)`;
+  hour_needle.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
+  minute_needle.style.transform = `rotate(${6 * minutes}deg)`;
   second_needle.style.transform = `rotate(${6 * seconds}deg)`;
 }, 10);
 setInterval(() => {
